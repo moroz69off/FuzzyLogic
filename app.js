b85159ce4c894eb0a0f532a5f53830886fcbf83c
Adam Theiner <adam.theiner@gmail.com>
2020-02-04 10:20:36 +0100

    fix, add new nets

--- app.js
+++ app.js
@@ -2,14 +2,37 @@
 
 var result = document.getElementById('result');
 var form = document.getElementById('form');
-// var resetButton = document.getElementById('sumbit');
+var vahaInput = document.getElementById('vaha');
+var algoInput = document.getElementById('algo');
+var mastnotaInput = document.getElementById('mastnota');
+var znecistenieInput = document.getElementById('znecistenie');
+
+
+
+function valueFromInputs() {
+  const inputValues = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ];
+
+  const vaha = inputValues[vahaInput.value];
+  const mastnota = inputValues[mastnotaInput.value];
+  const znecistenie = inputValues[znecistenieInput.value];
+
+  const inputs = [...vaha, ...mastnota, ...znecistenie];
+
+  return inputs;
+}
+
+
 
 function onSubmit(event) {
   event.preventDefault();
-  // todo
+  const algo = algoInput.value;
 
-  const value = 14.3;
-  result.innerText = value + ' min';
+  const value = calc(valueFromInputs(), algo);
+  result.innerText = value.toFixed(4) + ' min';
 }
 
 form.addEventListener('submit', onSubmit);
@@ -27,34 +50,40 @@ function stf(num) {
 }
 
 
-// inputs: [1, 0, 0, 1, ...]
-// net: NHL
-function calc(inputs, net) {
-  const matrix = getWeightMatrix(net);
-  const result = [];
+// inputs: [1, 0, 0, 1, 0, 0, 1, 0, 0]
+function calc(inputs, netId) {
+  const W = getWeightMatrix(netId);
+  inputs = new Array(22).fill(0).concat(inputs);
+  const o = [0, 1.8, 0, 20, 0, 15, 0, 0, 0, 15, 0, 1, 0, 0, 4.5, 0, 0, 15, 0, 0, 12.5, 0];
+  const outputs = o.concat(new Array(9).fill(0));
+  // const outputs = new Array(22).fill(stf(0)).concat(new Array(9).fill(0));
+
 
   let sum = 0;
 
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i]
-    const row = matrix[i];
-    const rowResult = [];
+  for (let j = 0; j < inputs.length; j++) {
+    let s = 0;
+    for (let i = 0; i < inputs.length; i++) {
+      if (i === j) continue;
 
-    for (let j = 0; j < row.length; j++) {
-      const value = inputs[i] * row[j];
-      rowResult.push(value);
-
-      sum += value;
+      const w = W[i][j];
+      const ci = inputs[i];
+      s += w * ci;
     }
 
-    result.push(rowResult);
+    outputs[j] = stf(s + outputs[j]);
   }
 
-  console.log(sum);
+  sum = outputs.slice(0, 22).reduce((a, b) => a + b);
 
-  return result;
+  return sum;
 }
 
-// 0.000000	0.700000	0.000000	0.000000	0.000000	0.888000	0.000000	0.583063	0.000000	0.000000
 
-calc([0, 0.7, 0, 0, 0, 0.888, 0, 0.583063, 0], 'NHL')
+function test() {
+  // calc([0, 0.7, 0, 0, 0, 0.888, 0, 0.583063, 0], 'NHL')
+  calc([0, 0, 1, 0, 0, 1, 0, 0, 1], 'NHL')
+  // calc([0.3, 0, 0, 0.115, 0, 0, 0, 0.33293, 0], 'NHL')
+}
+
+test();
