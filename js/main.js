6f5ec58487b3caaa0c3ddafde3e93b1bf0f24c7e
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-03 00:47:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 'use strict';
-var result = document.getElementById('result');
-var form = document.getElementById('form');
+var reset = document.getElementById('reset');
+var m_form = document.getElementById('m_form');
+var main_btn = document.getElementById('main');
+var table_result = document.getElementById('t_result');
+
+var compute_data = [];
+
+main_btn.onclick = Compute;
+
+function Compute () {
+	
+}
+
+function TableUpdate (compute_data) {
+	
+}
