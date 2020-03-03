5f1fdeaf27f788c88b1d21647db416da9f995fbf
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-03 04:11:09 +0300

    table updated

--- js/main.js
+++ js/main.js
@@ -2,16 +2,24 @@
 var reset = document.getElementById('reset');
 var m_form = document.getElementById('m_form');
 var main_btn = document.getElementById('main');
-var table_result = document.getElementById('t_result');
+var t_result = document.getElementById('t_result');
 
-var compute_data = [];
+var compute_data = 'a b c';
 
 main_btn.onclick = Compute;
 
 function Compute () {
-	
+	TableUpdate(compute_data);
 }
 
-function TableUpdate (compute_data) {
-	
+function TableUpdate (m_data) {
+	var t_data = m_data.split(' ');
+	for (var i=0; i<t_data.length; i++) {
+		var t_row = t_result.insertRow();
+		for (var j=0; j<3; j++) {
+			var data_cell = t_row.insertCell(j);
+			var text_cell = document.createTextNode(t_data[j]+i);
+			data_cell.appendChild(text_cell);
+		}
+	}
 }
