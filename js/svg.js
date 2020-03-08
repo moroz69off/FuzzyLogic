1d13db30af0896ef19965ae0f6405cce3d4d1279
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-08 15:10:43 +0300

    added function SetPointsCoordinates
      may be, 2 points collections: top points, bottom points

--- js/svg.js
+++ js/svg.js
@@ -5,21 +5,33 @@ svg.setAttributeNS(null, 'width', '100%');
 svg.setAttributeNS(null, 'height', '100%');
 div.appendChild(svg);
 
-var circ = document.createElementNS(ns, 'circle');
-circ.setAttributeNS(null, 'cx', 50);
-circ.setAttributeNS(null, 'cy', 30);
-circ.setAttributeNS(null, 'r', 10);
-circ.setAttributeNS(null, 'fill', '#0ff');
-svg.appendChild(circ);
+var Points = [];
 
 
-function CreatePoint (x, y) {
+function DrawPoint (x, y) {
 	var circ = document.createElementNS(ns, 'circle');
 	circ.setAttributeNS(null, 'cx', x);
 	circ.setAttributeNS(null, 'cy', y);
 	circ.setAttributeNS(null, 'r', 10);
-	circ.setAttributeNS(null, 'fill', '#0ff');
+	circ.setAttributeNS(null, 'fill', '#777');
 	svg.appendChild(circ);
 }
 
-CreatePoint(100,70);
+function SetPointsCoordinates () {
+	var svgFrameWidth = svg.clientWidth;
+	var svgFrameHeigth = svg.clientHeight;
+	var intervalPoint = svgFrameWidth/23;
+	for (var i = 0; i < 9; i++) {
+		var Point = new Object({x: 20 * (i + 1), y:20});
+		Points.push(Point);
+	}
+	console.log(Points);
+}
+
+SetPointsCoordinates();
+
+for (var i=0; i<Points.length; i++) {
+	DrawPoint(Points[i].x, Points[i].y);
+}
+
+
