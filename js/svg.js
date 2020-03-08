36af66046cfc18be30dc81d4274f16bc8a4cbca6
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-08 23:56:19 +0300

    point sizes adjusted

--- js/svg.js
+++ js/svg.js
@@ -5,32 +5,50 @@ svg.setAttributeNS(null, 'width', '100%');
 svg.setAttributeNS(null, 'height', '100%');
 div.appendChild(svg);
 
-var Points = [];
-
-
+var PointsTopper = [];
+var PointsBottom = [];
+var radius;
+//if (svg.clientWidth > 888) {radius = 8;} else {radius = svg.clientWidth / 60;}
+radius = svg.clientWidth / 64;
+console.log('svg.clientWidth: '+svg.clientWidth+', compute radius: '+(svg.clientWidth/64) +', radius: '+radius);                       //DEBUG
 function DrawPoint (x, y) {
-	var circ = document.createElementNS(ns, 'circle');
-	circ.setAttributeNS(null, 'cx', x);
-	circ.setAttributeNS(null, 'cy', y);
-	circ.setAttributeNS(null, 'r', 10);
-	circ.setAttributeNS(null, 'fill', '#777');
-	svg.appendChild(circ);
+	var circle = document.createElementNS(ns, 'circle');
+	circle.setAttributeNS(null, 'cx', x);
+	circle.setAttributeNS(null, 'cy', y);
+	circle.setAttributeNS(null, 'r', radius);
+	circle.setAttributeNS(null, 'fill', '#777');
+	svg.appendChild(circle);
 }
 
-function SetPointsCoordinates () {
+
+function SetPointsCoordinates (topPointsNumber, bottomPointsNumber) { // params: how many points in the top row, how many points in the bottom row
 	var svgFrameWidth = svg.clientWidth;
 	var svgFrameHeigth = svg.clientHeight;
-	var intervalPoint = svgFrameWidth/23;
-	for (var i = 0; i < 9; i++) {
-		var Point = new Object({x: (20 + intervalPoint) * (i + 1), y:20});
-		Points.push(Point);
+	var intervalOfPoints = svgFrameWidth/40 + radius; // distance between points
+	console.log('interval '+intervalOfPoints)                                // debug
+	for (var i = 0; i < topPointsNumber; i++) { 
+		var Point = new Object({x: intervalOfPoints * (i + 1), y: 20, name: 'top_point_' + (i + 1)});
+		PointsTopper.push(Point);
+	}
+	for (var i=0; i<bottomPointsNumber; i++) {
+		var Point = new Object({x: intervalOfPoints * (i + 1), y:svgFrameHeigth - 20, name: 'bottom_point_' + (i + 1)});
+		PointsBottom.push(Point);
+
 	}
 }
 
-SetPointsCoordinates();
+function DrawPoints () {
+	for (var i=0; i<PointsTopper.length; i++) {
+		DrawPoint(PointsTopper[i].x, PointsTopper[i].y);
+	}
 
-for (var i=0; i<Points.length; i++) {
-	DrawPoint(Points[i].x, Points[i].y);
+	for (var i=0; i<PointsBottom.length; i++) {
+		DrawPoint(PointsBottom[i].x, PointsBottom[i].y);
+	}
+
+	//Ofset();
 }
 
+SetPointsCoordinates(9, 22);
 
+DrawPoints();
