c750287528df6fbfb3c0d72190023b4c677f68bc
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-13 08:39:49 +0300

    Create common.js

---
+++ js/common.js
@@ -0,0 +1,14 @@
+var connectMatrix = [,]; //connection matrix (double)
+var compoundListingMatrix = [,];// compound matrix for listing (double)
+var learnParameterMatrix = [,]; // learning parameter matrix (double)
+var T_LearnExamples = [,]; // learning examples matrix (3000, 31), (double)
+
+fetch('data/trenovacka.txt')
+	.then(response => response.text())
+	.then(m_text => Trenovacka(m_text));
+
+function Trenovacka (text_arg) {
+	var data_points = text_arg.split('\n');
+	data_points.splice(0, 1);
+	alert(data_points[0]);
+}
