cf58156a06aeef43cb3122a0a3c008315a0ff141
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-13 17:07:36 +0300

    auxiliary set

--- js/common.js
+++ js/common.js
@@ -1,14 +1,32 @@
 var connectMatrix = [,]; //connection matrix (double)
 var compoundListingMatrix = [,];// compound matrix for listing (double)
 var learnParameterMatrix = [,]; // learning parameter matrix (double)
-var learnExamples = [,]; // learning examples matrix (3000, 31), (double)
+var T_LearnExamples = [,]; // learning examples matrix (3000, 31), (double)
+var Tclone = [[5,6],[1,2]]; //public static double[,] Tclone; //вспомогательный набор
 
-fetch('data/trenovacka.txt')
+function Inicializacia () {
+	//NacitajT();
+	//InicializujE();
+	//ExpertFCM();
+	Tclone = T.Clone();
+}
+
+fetch('data/trenovacka.txt') // get trenovacka data
 	.then(response => response.text())
 	.then(m_text => Trenovacka(m_text));
 
-function Trenovacka (text_arg) {
+function Trenovacka (text_arg) { //we a hepping get 3000 lines with training data
 	var data_points = text_arg.split('\n');
 	data_points.splice(0, 1);
-	console.log(data_points[0]);
+	//console.log(data_points[0]);
 }
+
+class T {
+	Clone(){
+		console.log(Tclone);
+	}
+}
+
+T = new T();
+
+Inicializacia ();
