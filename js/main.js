6ee121457db26e10b7967b70c7607c7d2ed57d43
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-17 10:28:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,15 @@ var result = document.getElementById('result');
 var option_learn_select = document.getElementById('learn_OptionSelect');
 var wash_WeightRange = document.getElementById('wash_Weight');
 var wash_Weight_span = document.getElementById('weight_option');
+var water_result_span = document.getElementById('water_result');
+
 var compute_data = 'a b c';
 
 compute_btn.onclick = Compute;
 
-wash_WeightRange.oninput = function () {
+wash_WeightRange.oninput = InputWeightRange;
+
+function InputWeightRange() {
 	wash_Weight_span.innerText = wash_WeightRange.value;
 	ComputeWater();
 }
