9d4b8dea44e988c86141fa4f5e619fd4e4892c47
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-17 12:07:41 +0300

    Update linearHebbEducation.js

--- js/linearHebbEducation.js
+++ js/linearHebbEducation.js
@@ -2,3 +2,8 @@ function ComputeWater () {
 	let water = parseInt(wash_WeightRange.value, 10) * 0.74 + 2.2;
 	water_result_span.innerText = water.toString().substring(0, 3) + ' liters';
 }
+
+function ConmputeLinearTimeWash () {
+	let wash_time = ["Short", "Very Short", "Long", "Medium", "Very Long"];
+
+}
