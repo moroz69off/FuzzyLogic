533ac69304f31bafa70cdd66480bb674649a3f11
Yuri Morozov <moroz69off@users.noreply.github.com>
2020-03-17 08:12:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,19 @@
 'use strict';
 var reset = document.getElementById('reset');
 var m_form = document.getElementById('m_form');
-var main_btn = document.getElementById('main');
-var t_result = document.getElementById('t_result');
+var compute_btn = document.getElementById('compute');
+var result = document.getElementById('result');
 var option_learn_select = document.getElementById('learn_OptionSelect');
-
+var wash_WeightRange = document.getElementById('wash_Weight');
+var wash_Weight_span = document.getElementById('weight_option');
 var compute_data = 'a b c';
 
-main_btn.onclick = Compute;
-reset.onclick = ClearTable;
+compute_btn.onclick = Compute;
+
+wash_WeightRange.oninput = function () {wash_Weight_span.innerText = wash_WeightRange.value;}
 
 function Compute () {
-	ClearTable();
-	TableUpdate(compute_data);
+	
 }
 
-function TableUpdate (m_data) {
-	var t_data = m_data.split(' ');
-	for (var i=0; i<t_data.length; i++) {
-		var t_row = t_result.insertRow();
-		for (var j=0; j<3; j++) {
 
-			var date = new Date();//temp
-			var t_st = date.getSeconds();//temp
-
-			var data_cell = t_row.insertCell(j);
-			var text_cell = document.createTextNode(
-					t_st+t_data[j]+(i+1)//temp
-				);
-			data_cell.appendChild(text_cell);
-		}
-	}
-}
-
-function ClearTable () {
-	for (var i=t_result.getElementsByTagName('tr').length -1; i; i--) {
-		t_result.deleteRow(i);
-	}
-}
